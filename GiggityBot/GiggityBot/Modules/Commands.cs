217c8bb2fb2603d4d7f7719b1b355c79c9884145
Naquino14 <naquino14@outlook.com>
2021-01-02 12:53:43 -0500

    haha yes

--- GiggityBot/GiggityBot/Modules/Commands.cs
+++ GiggityBot/GiggityBot/Modules/Commands.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GiggityBot.Modules
 {
-    class Class1
+    public class Commands : ModuleBase<SocketCommandContext>
     {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
     }
 }
