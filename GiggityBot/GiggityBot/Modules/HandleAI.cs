dc08e92cc84ed58e42ac95dbd75dfd9b1056254b
Naquino14 <naquino14@outlook.com>
2021-03-07 18:16:01 -0500

    Finished AI implementation (untested)
    testing to see if i screwed up github

--- GiggityBot/GiggityBot/Modules/HandleAI.cs
+++ GiggityBot/GiggityBot/Modules/HandleAI.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using GianDetectorML.ConsoleApp;
+using GiggityBotML;
+using GiggityBotML.ConsoleApp;
 
 namespace GiggityBot.Modules
 {
---
+++ GiggityBot/GiggityBotML.ConsoleApp/WhoIsThis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GiggityBotML.Model;
+
+namespace GiggityBotML.ConsoleApp
+{
+    public class WhoIsThis
+    {
+        public static string CompareImage(string pathToComparison)
+        {
+            try
+            {
+                ModelInput sampleData = new ModelInput()
+                {
+                    ImageSource = pathToComparison,
+                };
+
+                var predictionResult = ConsumeModel.Predict(sampleData);
+
+                string _result = "Prediction: " + predictionResult.Prediction + " | Scores: " + predictionResult.Score;
+                return _result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
